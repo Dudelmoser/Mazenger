c3afed2a3cbf0bdb5fc7a49b9899887229c5eec2
Jan David Kleiß <Jan David Klei>
2017-03-01 03:46:28 +0100

    Reworked root reducer (not yet enabled)

--- app/containers/App/model.js
+++ app/containers/App/model.js
@@ -11,7 +11,34 @@
 // MemeGenerator: memes
 // Chatbot: chatbot
 
-const model = {
+const initState = {
+  session: {
+    email: "",
+    password: "",
+    appState: null,
+    validEmails: [],
+    userID: null,
+    threadID: null,
+    isConnected: false,
+  },
+
+  // session
+  friends: [],
+  threads: {},
+  histories: {},
+  users: {},
+  typers: {},
+  inputs: {},
+
+  // persistent
+  emojis: {},
+  memes: {},
+  chatbot: {},
+  dictionary: {},
+  settings: {},
+};
+
+const stateExample = {
   isConnected: false,
 
   login: {
@@ -21,106 +48,104 @@ const model = {
     userID: null,
   },
 
-  session: {
-    threadID: null,
+  threadID: null,
 
-    friends: [{                                                       // getFriendsList()
-      name: "Zanger Janus",
+  friends: [{                                                       // getFriendsList()
+    name: "Zanger Janus",
+    firstName: "Zanger",
+    userID: "100001663847246",
+    profileUrl: "https://www.facebook.com/zanger.janus",
+    thumbSrc: "https://scontent.ftxl1-1.fna.fbcdn.net/...",
+    isBirthday: false,
+  }],
+
+  users: {
+    "userID": {
+      fullName: "Zanger Janus",
       firstName: "Zanger",
-      userID: "100001663847246",
       profileUrl: "https://www.facebook.com/zanger.janus",
-      thumbSrc: "https://scontent.ftxl1-1.fna.fbcdn.net/...",
+      profilePicture: "https://scontent.ftxl1-1.fna.fbcdn.net/...",
       isBirthday: false,
+    },
+  },
+
+  threads: {
+    "threadID": {                                            // getThreadList(); getThreadInfo()
+      snippet: "Hello",
+      nicknames: {"100001663847246": "ZJ"},
+      participantIDs: ["100001663847246", "100000719100515", "100004684600778"],
+      messageCount: 22,
+      timestamp: 1488059498031,
+      lastReadTimestamp: 1488059498031,                             // getThreadInfo only
+      name: "Friends",                                              // getThreadInfo only
+      snippetSender: "100004684600778",                             // getThreadList only
+      snippetAttachments: [/*attachments*/],                        // getThreadList only
+      unreadCount: 0,                                               // getThreadList only
+      isArchived: false,                                            // getThreadList only
+      isCanonical: false,                                           // getThreadList only
+      isSubscribed: true,                                           // getThreadList only
+    },
+  },
+
+  outbox: {
+    "threadID": ""
+  },
+
+  typers: {
+    "threadID": {"100001663847246": 1488059498031}
+  },
+
+  history: {
+    "threadID": [{
+        messageID: "mid.1466685572194:30d37fc836dd59e621",
+        body: "uiuiui",
+        timestamp: 1466685572202,
+        timestampRelative: "23.06.2016",
+        timestampAbsolute: "23. Juni 2016",
+        timestampDatetime: "23. Juni 2016 14:39",
+        senderID: "fbid:100004684600778",
+        senderName: "Zanger Janus",
+        participantNames: ["Zanger"],
+        threadID: "100002096958364",
+        isGroup: false,
+        attachments: [{
+            type: "file",
+            name: "audioclip-1466694308035-4828.mp4",
+            url: "https://cdn.fbsbx.com/...",
+          },{
+            type: "share",
+            title: "How to Disable Windows 10 Data Collection - BestVPN.com",
+            description: "Since its release on July 29,...",
+            facebookUrl: "https://l.facebook.com/l.php?...",
+            image: "https://external.ftxl1-1.fna.fbcdn.net/...",
+            source: "bestvpn.com",
+            width: 826,
+            height: 418,
+            playable: false,
+          },{
+            type: "photo",
+            thumbnailUrl: "https://scontent.ftxl1-1.fna.fbcdn.net/...",
+            width: "960",
+            height: "540",
+            largePreviewUrl: "https://scontent.ftxl1-1.fna.fbcdn.net/...",
+            largePreviewWidth: 853,
+            largePreviewHeight: 480,
+            previewUrl: "https://scontent.ftxl1-1.fna.fbcdn.net/...",
+            previewWidth: 497,
+            previewHeight: 280,
+          }],
     }],
+  },
 
-    users: {
-      "100001663847246": {
-        firstName: "Zanger",
-        fullName: "Zanger Janus",
-        profileUrl: "https://www.facebook.com/zanger.janus",
-        profilePicture: "https://scontent.ftxl1-1.fna.fbcdn.net/...",
-        isBirthday: false,
-      },
-    },
-
-    threads: {
-      "100002096958364": {                                            // getThreadList(); getThreadInfo()
-        snippet: "Hello",
-        nicknames: {"100001663847246": "ZJ"},
-        participantIDs: ["100001663847246", "100000719100515", "100004684600778"],
-        messageCount: 22,
-        timestamp: 1488059498031,
-        lastReadTimestamp: 1488059498031,                             // getThreadInfo only
-        name: "Friends",                                              // getThreadInfo only
-        snippetSender: "100004684600778",                             // getThreadList only
-        snippetAttachments: [/*attachments*/],                        // getThreadList only
-        unreadCount: 0,                                               // getThreadList only
-        isArchived: false,                                            // getThreadList only
-        isCanonical: false,                                           // getThreadList only
-        isSubscribed: true,                                           // getThreadList only
-      },
-    },
-
-    outbox: {
-      "100002096958364": ""
-    },
-
-    typers: {
-      "100002096958364": {"100001663847246": 1488059498031}
-    },
-
-    history: {
-      "100002096958364": [{
-          messageID: "mid.1466685572194:30d37fc836dd59e621",
-          body: "uiuiui",
-          timestamp: 1466685572202,
-          timestampRelative: "23.06.2016",
-          timestampAbsolute: "23. Juni 2016",
-          timestampDatetime: "23. Juni 2016 14:39",
-          senderID: "fbid:100004684600778",
-          senderName: "Zanger Janus",
-          participantNames: ["Zanger"],
-          threadID: "100002096958364",
-          isGroup: false,
-          attachments: [{
-              type: "file",
-              name: "audioclip-1466694308035-4828.mp4",
-              url: "https://cdn.fbsbx.com/...",
-            },{
-              type: "share",
-              title: "How to Disable Windows 10 Data Collection - BestVPN.com",
-              description: "Since its release on July 29,...",
-              facebookUrl: "https://l.facebook.com/l.php?...",
-              image: "https://external.ftxl1-1.fna.fbcdn.net/...",
-              source: "bestvpn.com",
-              width: 826,
-              height: 418,
-              playable: false,
-            },{
-              type: "photo",
-              thumbnailUrl: "https://scontent.ftxl1-1.fna.fbcdn.net/...",
-              width: "960",
-              height: "540",
-              largePreviewUrl: "https://scontent.ftxl1-1.fna.fbcdn.net/...",
-              largePreviewWidth: 853,
-              largePreviewHeight: 480,
-              previewUrl: "https://scontent.ftxl1-1.fna.fbcdn.net/...",
-              previewWidth: 497,
-              previewHeight: 280,
-            }],
-      }],
+  emojis: {
+    "userID": {
+      favorites: {},
+      openGroups: [],
     }
   },
 
-  "100004684600778": {
-    friends: [],
-
-    emojis: {
-      favorites: {},
-      openGroups: [],
-    },
-
-    memes: {
+  memes: {
+    "userID": {
       top100: [],
       local: [],
       faves: [],
@@ -128,9 +153,11 @@ const model = {
       rendered: "",
       header: "",
       footer: "",
-    },
+    }
+  },
 
-    chatbot: {
+  chatbot: {
+    "userID": {
       "threadID": {
         enabled: false,
         dictionary: [
@@ -140,10 +167,14 @@ const model = {
           [1488064245575, "message"],
         ],
       }
-    },
+    }
+  },
 
-    dictionary: {},
+  dictionary: {
+    "userID": {}
+  },
 
-    settings: {},
+  settings: {
+    "userID": {}
   }
 };
--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -8,9 +8,9 @@ import friendsReducer from "../FriendsList/reducer";
 import memeReducer from "../MemeGenerator/reducer";
 
 import {initialState} from "./state";
-import {fromJS} from "immutable";
+import {Map, fromJS} from "immutable";
 
-function combineReducers(state = initialState, action) {
+function combinedReducer(state = initialState, action) {
   const reducers = [sessionReducer, updateReducer, loginReducer, threadsReducer, friendsReducer, inputReducer,
     emojisReducer, memeReducer];
   for (let reducer of reducers) {
@@ -21,15 +21,63 @@ function combineReducers(state = initialState, action) {
   return state;
 }
 
-function sliceReducer(state = fromJS({}), action) {
-  const reducers = [sessionReducer, updateReducer, loginReducer, threadsReducer, friendsReducer, inputReducer,
-    emojisReducer, memeReducer];
-  for (let reducer of reducers) {
-    const newState = reducer(state, action);
-    if (newState)
-      return newState;
-  }
-  return state;
+function combineReducers(reducers, params = []) {
+  const keys = Object.keys(reducers);
+
+  return (inputState = Map(), action) => inputState
+      .withMutations((tempState) => {
+        keys.forEach((key) => {
+          const reducer = reducers[key];
+          const curSubState = tempState.get(key);
+          const nextSubState = reducer(curSubState, action, ...params);
+          if (nextSubState)
+            tempState.set(key, nextSubState);
+        });
+      });
 }
 
-export default combineReducers;
+const initState = fromJS({
+  session: {
+    email: "",
+    password: "",
+    appState: null,
+    validEmails: [],
+    userID: null,
+    threadID: null,
+    isConnected: false,
+  },
+
+  // session
+  friends: [],
+  threads: {},
+  histories: {},
+  users: {},
+  typers: {},
+  inputs: {},
+
+  // persistent
+  emojis: {},
+  memes: {},
+  chatbot: {},
+  dictionary: {},
+  settings: {},
+});
+
+function sliceReducer(state = initState, action) {
+  const userID = state.getIn(["session", "userID"]);
+  const threadID = state.getIn(["session", "threadID"]);
+
+  const rootReducer = combineReducers({
+    session: loginReducer,
+    friends: friendsReducer,
+    threads: threadsReducer,
+    histories: historiesReducer,
+    users: usersReducer,
+    typers: typersReducer,
+    inputs: inputReducer,
+  }, [userID, threadID]);
+
+  return rootReducer(state, action)
+}
+
+export default combinedReducer;
